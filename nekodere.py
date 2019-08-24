9c4b2dc0cc7d4085717332f7eae3f07195892985
libbies <bcw@well.com>
2019-08-24 22:09:07 +0000

    the sopel frontend module for opendere

---
+++ nekodere.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# coding=utf-8
+"""nekodere sopel module, running opendere"""
+# Licensed under MIT or something idk ask kitties
+
+from sopel import tools
+from sopel.module import commands, interval, rule, example
+
+import sys
+sys.path.append('/home/libbies/.sopel/modules')
+
+import opendere.game
+import opendere.roles
+
+def setup(bot=None):
+    if not bot:
+        return
+    bot.memory['allowed_channels'] = ['#opendere', '##libbies-test']
+    bot.memory['nekodere'] = None
+
+@commands('nekodere')
+@example('!nekodere - start a new game')
+def join_game(bot, trigger):
+    """
+    join an existing (or create a new) nekodere instance
+    """
+    if trigger.sender not in bot.memory['allowed_channels']:
+        bot.say(f"you can only join or start a game from {' or '.join(bot.memory['allowed_channels'])}") 
+        return
+    if not bot.memory['nekodere']:
+        bot.memory['nekodere'] = opendere.game.Game('nekodere', trigger.sender, prefix='!')
+    for msg in bot.memory['nekodere'].join_game(trigger.nick, trigger.hostmask):
+        recipient, text = msg
+        if recipient in bot.memory['allowed_channels']:
+            bot.say('\x02' + text + '\x0f', recipient)
+        else:
+            bot.notice(text, recipient)
+
+@commands('hurry|nekodere hurry')
+@example('!hurry - hurry the current phase')
+def hurry(bot, trigger):
+    if not bot.memory['nekodere']:
+        return
+    for msg in bot.memory['nekodere'].user_hurry(trigger.nick):
+        recipient, text = msg
+        if recipient in bot.memory['allowed_channels']:
+            bot.say('\x02' + text + '\x0f', recipient)
+        else:
+            bot.notice(text, recipient)
+
+@commands('vote|nekodere vote')
+@example('!vote <target> - vote to kill someone')
+def vote(bot, trigger):
+    if not bot.memory['nekodere']:
+        return
+    for msg in bot.memory['nekodere'].user_action(trigger.nick, 'vote to kill'):
+        recipient, text = msg
+        if recipient in bot.memory['allowed_channels']:
+            bot.say('\x02' + text + '\x0f', recipient)
+        else:
+            bot.notice(text, recipient)
+
+@interval(1)
+def tick(bot):
+    """
+    tick down the timer for game state, i.e. the start timer or hurry timer
+    """
+    if not bot.memory['nekodere']:
+        return
+    bot.memory['nekodere'].tick()
