36306af7e97e46c2ec5306056de3abc5b22166af
andrew <andrew>
2019-08-25 18:11:31 -0400

    split up and fix tests

--- test/test_game.py
+++ test/test_game.py
@@ -1,23 +1,16 @@
 import pytest
 
-from opendere import game, roles
-
-
-def test_roles_dont_share_ability_objects():
-    hik0 = roles.Hikikomori()
-    hik1 = roles.Hikikomori()
-    assert len(hik0.abilities) == len(hik1.abilities)
-    hik0.abilities += ['foo']
-    assert len(hik0.abilities) != len(hik1.abilities)
+from opendere import game
 
 
 def test_create_game_too_few():
-    with pytest.raises(ValueError):
-        game.Game(['a'])
-    with pytest.raises(ValueError):
-        game.Game(['a', 'b'])
-    with pytest.raises(ValueError):
-        game.Game(['a', 'b', 'c'])
+    g = game.Game()
+    g.join_game('a', 'a')
+    g.join_game('b', 'b')
+    assert len(g.users) == 2
+    for _ in range(61):
+        g.tick()
+        assert len(g.users) == 0
 
 
 def test_create_game_success():
